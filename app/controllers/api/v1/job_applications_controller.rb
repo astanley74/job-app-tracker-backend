1630f71cd70d580783d93c33edd93f6f17f5e9ed
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-19 18:00:10 -0400

    add actions to job_applications controller

--- app/controllers/api/v1/job_applications_controller.rb
+++ app/controllers/api/v1/job_applications_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::JobApplicationsController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+        def user_params
+        end
+
 end
