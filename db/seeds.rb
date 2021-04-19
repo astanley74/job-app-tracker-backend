c712fd4a4d6758374c9305b0432459141997223f
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-19 17:11:53 -0400

    add seed data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(first_name: "Austin", last_name: "Franklin", email: "austinfranklin@gmail.com")
+job_app = JobApplication.create(company_name: "Apple", date_of_application: Date.today, position: "Software Engineer", application_status: true, current_stage: "Phone Interview", notes: "Great Company", user_id: user.id)
