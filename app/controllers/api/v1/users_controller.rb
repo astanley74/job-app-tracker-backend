640797af9481c9f3ed32efc7c76c813a1d7bf9dd
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-20 00:10:49 -0400

    add functionality to user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,13 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
+        if User.find_by(:email => user_params[:email])
+            user = User.find_by(:email => user_params[:email])
+            render json: user
+        else
+            user = User.crerate(user_params)
+            render json: user
+        end
     end
 
     def show
@@ -16,6 +23,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
         def user_params
+            params.require(:user).permit(:first_name, :last_name, :email)
         end
 
 end
