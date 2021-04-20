984a167cb827be8626f4e23761f9cbf31c5fed33
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-20 15:57:43 -0400

    add id attribute to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :first_name, :last_name, :email
+    attributes :id, :first_name, :last_name, :email
     has_many :job_applications
 end
