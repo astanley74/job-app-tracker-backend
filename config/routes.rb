2ac5ac20d2cd02b8f01d91c0301d2d110f75cd44
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-21 11:13:42 -0400

    add nested route for job_applications

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
-      resources :job_applications
+      resources :users do
+        resources :job_applications
+      end
     end
   end
   
