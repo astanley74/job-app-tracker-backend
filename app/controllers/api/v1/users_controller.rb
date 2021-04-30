8014af9644cb29cb8614ddbb844dca0bac34ed79
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-30 17:44:03 -0400

    refactor code in job app controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
         def user_params
-            params.require(:user).permit(:first_name, :last_name, :email)
+            params.require(:user).permit(:email, :first_name, :last_name)
         end
 
 end
