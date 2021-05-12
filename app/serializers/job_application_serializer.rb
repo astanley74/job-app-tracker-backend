5742c62ee52aeb4207d54783cd40d129228ddbfc
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-05-11 20:55:38 -0400

    add belongs to association in serializer

--- app/serializers/job_application_serializer.rb
+++ app/serializers/job_application_serializer.rb
@@ -1,3 +1,4 @@
 class JobApplicationSerializer < ActiveModel::Serializer
     attributes :id, :company_name, :date_of_application, :position, :application_status, :current_stage, :notes, :user_id
+    belongs_to :user
 end
