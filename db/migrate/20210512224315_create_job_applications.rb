838f9280258bc8ab4b8fe6acecc8effd7095eff7
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-05-12 18:46:50 -0400

    drop database due to issues and recreate

--- db/migrate/20210512224315_create_job_applications.rb
+++ db/migrate/20210512224315_create_job_applications.rb
@@ -2,7 +2,7 @@ class CreateJobApplications < ActiveRecord::Migration[6.1]
   def change
     create_table :job_applications do |t|
       t.string :company_name
-      t.date :date_of_application
+      t.string :date_of_application
       t.string :position
       t.boolean :application_status
       t.string :current_stage
