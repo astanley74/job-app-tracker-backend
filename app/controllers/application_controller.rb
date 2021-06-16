8f2627366ddba39071b28801a0647bb193306b6a
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-06-16 14:31:32 -0400

    add .env file and begin JWT implementation for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload, ENV["SECRET_KEY"])
+    end
+
+    def decoded_token(token)
+        JWT.decode(token, ENV["SECRET_KEY"])[0]
+    end
 end
