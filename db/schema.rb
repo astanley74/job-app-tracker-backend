73e58c2dfc374a62afc561ce282674edbb562d49
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-06-15 23:29:09 -0400

    add password to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_224514) do
+ActiveRecord::Schema.define(version: 2021_06_16_032807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_05_12_224514) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "job_applications", "users"
